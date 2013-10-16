99a2ca77f362e19a96694e53a42a2dbcdb950783
eirslett <eiriksletteberg@gmail.com>
2013-10-16 19:05:08 +0200

    Add example for how you can override karma config with a new javascript config file

---
+++ example/src/test/javascript/karma.conf.ci.js
@@ -0,0 +1,11 @@
+var baseConfig = require('./karma.conf.js');
+
+module.exports = function(config){
+    // Load base config
+    baseConfig(config);
+
+    // Override base config
+    config.set({
+        singleRun: true
+    });
+};
