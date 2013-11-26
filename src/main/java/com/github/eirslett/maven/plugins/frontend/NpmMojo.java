c3cce65d71b40b4f55908d92213149e0fde8dceb
eirslett <eiriksletteberg@gmail.com>
2013-11-26 12:13:02 +0100

    Rename command -> arguments

--- src/main/java/com/github/eirslett/maven/plugins/frontend/NpmMojo.java
+++ src/main/java/com/github/eirslett/maven/plugins/frontend/NpmMojo.java
@@ -2,6 +2,8 @@ package com.github.eirslett.maven.plugins.frontend;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -16,16 +18,22 @@ public final class NpmMojo extends AbstractMojo {
     @Parameter(defaultValue = "${basedir}", property = "workingDirectory", required = false)
     private File workingDirectory;
 
-    @Parameter(defaultValue = "install", property = "command", required = false)
-    private String command;
+    @Parameter(defaultValue = "install", property = "arguments", required = false)
+    private String arguments;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Running NPM '" + command + "' in "+workingDirectory.toString());
-        final String npmPath = workingDirectory+"/node/npm/bin/npm-cli.js".replace("/", File.separator);
-        int result = new NodeExecutor(workingDirectory, Arrays.asList(npmPath, command)).executeAndRedirectOutput(getLog());
+        getLog().info("Running 'npm " + arguments + "' in "+workingDirectory.toString());
+        final String npmPath = workingDirectory + normalize("/node/npm/bin/npm-cli.js");
+
+        List<String> commands = new LinkedList<String>(Arrays.asList(npmPath));
+        if(arguments != null && !arguments.equals("null") && !arguments.isEmpty()) {
+            commands.addAll(Arrays.asList(arguments.split("\\s+")));
+        }
+
+        int result = new NodeExecutor(workingDirectory, commands).executeAndRedirectOutput(getLog());
         if(result != 0){
-            throw new MojoFailureException("NPM '" + command + "' failed.");
+            throw new MojoFailureException("'npm "+arguments+"' failed.");
         }
     }
 }
