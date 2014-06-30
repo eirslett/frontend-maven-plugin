da987c5e85cc9a2a827f2ae2e5d68fb78799b77a
eirslett <eirik.sletteberg@finn.no>
2014-06-30 11:48:23 +0200

    gradle plugin - WIP 2

--- frontend-gradle-plugin/src/main/groovy/com/github/eirslett/gradle/frontend/FrontendPlugin.groovy
+++ frontend-gradle-plugin/src/main/groovy/com/github/eirslett/gradle/frontend/FrontendPlugin.groovy
@@ -1,4 +1,25 @@
-package com.github.eirslett.gradle.frontend;
+package com.github.eirslett.gradle.frontend
 
-public final class FrontendPlugin {
+import com.github.eirslett.maven.plugins.frontend.lib.InstallationException
+import org.gradle.api.GradleException
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import com.github.eirslett.maven.plugins.frontend.lib.FrontendPluginFactory
+
+public final class FrontendPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.task('install-node-and-npm') {
+            File buildDir = project.getBuildDir()
+            nodeVersion = project.frontend.nodeVersion
+            npmVersion = project.frontend.npmVersion
+            try {
+                println buildDir
+                new FrontendPluginFactory(buildDir).getNodeAndNPMInstaller().install(nodeVersion, npmVersion, 'http://nodejs.org/dist/')
+            } catch (InstallationException e) {
+                throw new GradleException(e.getLocalizedMessage())
+            }
+        }
+    }
 }
