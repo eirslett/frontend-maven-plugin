e4cc462d55318890f722a2ec6634debfd9ca32ab
Malone <seamus@flurry.com>
2014-07-09 21:57:55 -0700

    added groc and webdriver-mananger

---
+++ frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/GrocMojo.java
@@ -0,0 +1,7 @@
+package com.github.eirslett.maven.plugins.frontend.mojo;
+
+/**
+ * Created by Malone on 7/7/14.
+ */
+public class GrocMojo {
+}
---
+++ frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/GrocRunner.java
@@ -0,0 +1,7 @@
+package com.github.eirslett.maven.plugins.frontend.lib;
+
+/**
+ * Created by Malone on 7/7/14.
+ */
+public class GrocRunner {
+}
