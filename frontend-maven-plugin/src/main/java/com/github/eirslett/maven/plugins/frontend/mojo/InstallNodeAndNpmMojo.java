d68638a531c52e52b29c178f9458354f5b90794f
Lukas Fryc <lfryc@redhat.com>
2014-08-15 12:28:04 +0200

    allow to defined Node and NPM download roots independently - fixes #65

--- frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/InstallNodeAndNpmMojo.java
+++ frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/InstallNodeAndNpmMojo.java
@@ -1,8 +1,7 @@
 package com.github.eirslett.maven.plugins.frontend.mojo;
 
-import com.github.eirslett.maven.plugins.frontend.lib.FrontendPluginFactory;
-import com.github.eirslett.maven.plugins.frontend.lib.InstallationException;
-import com.github.eirslett.maven.plugins.frontend.lib.ProxyConfig;
+import java.io.File;
+
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -11,7 +10,10 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.io.File;
+import com.github.eirslett.maven.plugins.frontend.lib.FrontendPluginFactory;
+import com.github.eirslett.maven.plugins.frontend.lib.InstallationException;
+import com.github.eirslett.maven.plugins.frontend.lib.NodeAndNPMInstaller;
+import com.github.eirslett.maven.plugins.frontend.lib.ProxyConfig;
 
 @Mojo(name="install-node-and-npm", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
 public final class InstallNodeAndNpmMojo extends AbstractMojo {
@@ -23,9 +25,24 @@ public final class InstallNodeAndNpmMojo extends AbstractMojo {
     private File workingDirectory;
 
     /**
-     * The base directory for running all Node commands. (Usually the directory that contains package.json)
+     * Where to download Node.js binary from. Defaults to http://nodejs.org/dist/
+     */
+    @Parameter(property = "nodeDownloadRoot", required = false, defaultValue = NodeAndNPMInstaller.DEFAULT_NODEJS_DOWNLOAD_ROOT)
+    private String nodeDownloadRoot;
+
+    /**
+     * Where to download NPM binary from. Defaults to http://registry.npmjs.org/npm/-/
+     */
+    @Parameter(property = "npmDownloadRoot", required = false, defaultValue = NodeAndNPMInstaller.DEFAULT_NPM_DOWNLOAD_ROOT)
+    private String npmDownloadRoot;
+
+    /**
+     * Where to download Node.js and NPM binaries from.
+     *
+     * @deprecated use {@link #nodeDownloadRoot} and {@link #npmDownloadRoot} instead, this configuration will be used only when no {@link #nodeDownloadRoot} or {@link #npmDownloadRoot} is specified.
      */
     @Parameter(property = "downloadRoot", required = false, defaultValue = "")
+    @Deprecated
     private String downloadRoot;
 
     /**
@@ -48,9 +65,25 @@ public final class InstallNodeAndNpmMojo extends AbstractMojo {
         try {
             MojoUtils.setSLF4jLogger(getLog());
             ProxyConfig proxyConfig = MojoUtils.getProxyConfig(session);
-            new FrontendPluginFactory(workingDirectory, proxyConfig).getNodeAndNPMInstaller().install(nodeVersion, npmVersion, downloadRoot);
+            String nodeDownloadRoot = getNodeDownloadRoot();
+            String npmDownloadRoot = getNpmDownloadRoot();
+            new FrontendPluginFactory(workingDirectory, proxyConfig).getNodeAndNPMInstaller().install(nodeVersion, npmVersion, nodeDownloadRoot, npmDownloadRoot);
         } catch (InstallationException e) {
             throw MojoUtils.toMojoFailureException(e);
         }
+    }
+
+    private String getNodeDownloadRoot() {
+        if (downloadRoot != null && !"".equals(downloadRoot) && NodeAndNPMInstaller.DEFAULT_NODEJS_DOWNLOAD_ROOT.equals(nodeDownloadRoot)) {
+            return downloadRoot;
+        }
+        return nodeDownloadRoot;
+    }
+
+    private String getNpmDownloadRoot() {
+        if (downloadRoot != null && !"".equals(downloadRoot) && NodeAndNPMInstaller.DEFAULT_NPM_DOWNLOAD_ROOT.equals(npmDownloadRoot)) {
+            return downloadRoot;
+        }
+        return npmDownloadRoot;
     }
 }
