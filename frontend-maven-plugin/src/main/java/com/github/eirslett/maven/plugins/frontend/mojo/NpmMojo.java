fef719c7efbbceb887c13048a3c3924c3e826553
Karl von Randow <karl@xk72.com>
2014-11-03 09:26:27 +1300

    M2E support
    Including incremental build modified files checking. Refreshing built files.
    
    Adds triggerfiles, srcdir, outputdir parameters to the Grunt and Gulp mojos.

--- frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/NpmMojo.java
+++ frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/NpmMojo.java
@@ -5,13 +5,16 @@ import java.io.File;
 import com.github.eirslett.maven.plugins.frontend.lib.FrontendPluginFactory;
 import com.github.eirslett.maven.plugins.frontend.lib.ProxyConfig;
 import com.github.eirslett.maven.plugins.frontend.lib.TaskRunnerException;
+
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.sonatype.plexus.build.incremental.BuildContext;
 
 import static com.github.eirslett.maven.plugins.frontend.mojo.MojoUtils.setSLF4jLogger;
 
@@ -32,17 +35,24 @@ public final class NpmMojo extends AbstractMojo {
 
     @Parameter(property = "session", defaultValue = "${session}", readonly = true)
     private MavenSession session;
+    
+    @Component
+    private BuildContext buildContext;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        try {
-            setSLF4jLogger(getLog());
+        File packageJson = new File(workingDirectory, "package.json");
+        if (buildContext == null || buildContext.hasDelta(packageJson) || !buildContext.isIncremental()) {
+            try {
+                setSLF4jLogger(getLog());
 
-            ProxyConfig proxyConfig = MojoUtils.getProxyConfig(session);
-            new FrontendPluginFactory(workingDirectory, proxyConfig).getNpmRunner()
-                    .execute(arguments);
-        } catch (TaskRunnerException e) {
-            throw new MojoFailureException("Failed to run task", e);
+                ProxyConfig proxyConfig = MojoUtils.getProxyConfig(session);
+                new FrontendPluginFactory(workingDirectory, proxyConfig).getNpmRunner().execute(arguments);
+            } catch (TaskRunnerException e) {
+                throw new MojoFailureException("Failed to run task", e);
+            }
+        } else {
+            getLog().info("Skipping npm install as package.json unchanged");
         }
     }
 }
