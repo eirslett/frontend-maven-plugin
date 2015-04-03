c908bc372e1e2e5bb7421543871561e32e02243e
Scott Busche <swbusche@gmail.com>
2015-04-03 10:46:05 -0500

    [Fix #190] Bower doesn't pass down proxy settings
    Note our corporate proxy uses only http was not able to test https, but it's mirrored after the NPM settings

--- frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/BowerMojo.java
+++ frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/BowerMojo.java
@@ -1,13 +1,17 @@
 package com.github.eirslett.maven.plugins.frontend.mojo;
 
 import com.github.eirslett.maven.plugins.frontend.lib.FrontendPluginFactory;
+import com.github.eirslett.maven.plugins.frontend.lib.ProxyConfig;
 import com.github.eirslett.maven.plugins.frontend.lib.TaskRunnerException;
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.settings.crypto.SettingsDecrypter;
 
 import java.io.File;
 
@@ -26,6 +30,12 @@ public final class BowerMojo extends AbstractMojo {
     @Parameter(defaultValue = "install", property = "frontend.bower.arguments", required = false)
     private String arguments;
 
+    @Parameter(property = "session", defaultValue = "${session}", readonly = true)
+    private MavenSession session;
+
+    @Component(role = SettingsDecrypter.class)
+    private SettingsDecrypter decrypter;
+
     /**
      * Skips execution of this mojo.
      */
@@ -36,7 +46,8 @@ public final class BowerMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException, MojoFailureException {
         if(!skip) {
             try {
-                new FrontendPluginFactory(workingDirectory).getBowerRunner().execute(arguments);
+                ProxyConfig proxyConfig = MojoUtils.getProxyConfig(session, decrypter);
+                new FrontendPluginFactory(workingDirectory, proxyConfig).getBowerRunner().execute(arguments);
             } catch (TaskRunnerException e) {
                 throw new MojoFailureException("Failed to run task", e);
             }
