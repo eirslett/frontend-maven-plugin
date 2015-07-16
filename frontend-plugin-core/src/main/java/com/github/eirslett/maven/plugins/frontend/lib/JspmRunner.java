8327d0786826cd7a8b2753d571ff2f7da5744c29
Simon White <simon.white@melbourneit.com.au>
2015-07-17 09:26:08 +1000

    Updated to reflect API changes.

--- frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/JspmRunner.java
+++ frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/JspmRunner.java
@@ -1,21 +1,16 @@
 package com.github.eirslett.maven.plugins.frontend.lib;
 
-import java.io.File;
-import java.util.ArrayList;
-
 public interface JspmRunner {
-
-    public void execute(String args) throws TaskRunnerException;
+    void execute(String args) throws TaskRunnerException;
 }
 
 
 final class DefaultJspmRunner extends NodeTaskExecutor implements JspmRunner {
 
-    static final String TASK_NAME = "jspm";
-    static final String TASK_LOCATION = "/node_modules/jspm/jspm.js";
+    static final String TASK_LOCATION = "node_modules/jspm/jspm.js";
 
-    public DefaultJspmRunner(Platform platform, File workingDirectory) {
-        super(TASK_NAME, TASK_LOCATION, workingDirectory, platform, new ArrayList<String>());
+    DefaultJspmRunner(NodeExecutorConfig config) {
+        super(config, TASK_LOCATION);
     }
 
 }
