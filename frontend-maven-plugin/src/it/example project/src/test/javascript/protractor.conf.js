02fd157d8884fe27f34ac7001482a94f5e1efd46
Stefano Vetrini <s.vetrini@consulthink.it>
2017-01-10 15:14:44 +0100

    bugfix build error on travis.ci

--- frontend-maven-plugin/src/it/example project/src/test/javascript/protractor.conf.js
+++ frontend-maven-plugin/src/it/example project/src/test/javascript/protractor.conf.js
@@ -1,7 +1,19 @@
 exports.config = {
   multiCapabilities: [
 	{
-		browserName: 'chrome',
+		  'browserName': 'phantomjs',
+
+		  /* 
+		   * Can be used to specify the phantomjs binary path.
+		   * This can generally be ommitted if you installed phantomjs globally.
+		   */
+		  'phantomjs.binary.path': require('phantomjs').path,
+
+		  /*
+		   * Command line args to pass to ghostdriver, phantomjs's browser driver.
+		   * See https://github.com/detro/ghostdriver#faq
+		   */
+		  'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
 	}
   ], 
   specs: [
