48ffec60f58db563e895b2e3ef5614cadf692cf6
Bojan Tomic <bojan.tomic@servicenow.com>
2017-02-10 01:38:32 +0100

    closes #534: Do not fail the Maven build for a failed test  Added support for testFailureIgnore flag as in Surefire plugin

--- frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/KarmaRunMojo.java
+++ frontend-maven-plugin/src/main/java/com/github/eirslett/maven/plugins/frontend/mojo/KarmaRunMojo.java
@@ -2,10 +2,10 @@ package com.github.eirslett.maven.plugins.frontend.mojo;
 
 import com.github.eirslett.maven.plugins.frontend.lib.FrontendPluginFactory;
 import com.github.eirslett.maven.plugins.frontend.lib.TaskRunnerException;
+
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.slf4j.LoggerFactory;
 
 
 @Mojo(name="karma",  defaultPhase = LifecyclePhase.TEST)
@@ -16,12 +16,6 @@ public final class KarmaRunMojo extends AbstractFrontendMojo {
      */
     @Parameter(defaultValue = "karma.conf.js", property = "karmaConfPath")
     private String karmaConfPath;
-
-    /**
-     * Whether you should continue build when some test will fail (default is false)
-     */
-    @Parameter(property = "maven.test.failure.ignore", required = false, defaultValue = "false")
-    private Boolean testFailureIgnore;
 
     /**
      * Skips execution of this mojo.
@@ -36,17 +30,6 @@ public final class KarmaRunMojo extends AbstractFrontendMojo {
 
     @Override
     public void execute(FrontendPluginFactory factory) throws TaskRunnerException {
-        try {
-            factory.getKarmaRunner().execute("start " + karmaConfPath, environmentVariables);
-        }
-        catch (TaskRunnerException e) {
-            if (testFailureIgnore) {
-                LoggerFactory.getLogger(KarmaRunMojo.class)
-                        .warn("There are ignored test failures/errors for: " + workingDirectory);
-            }
-            else {
-                throw e;
-            }
-        }
+        factory.getKarmaRunner().execute("start " + karmaConfPath, environmentVariables);
     }
 }
