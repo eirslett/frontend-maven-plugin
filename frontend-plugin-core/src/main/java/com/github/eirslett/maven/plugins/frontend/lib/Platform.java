4a7cf7b8ee4646124ae0926123e2f8c37cbee38f
kadamvandita <vandita_kadam@persistent.com>
2017-07-11 02:58:55 -0400

    adding s390x support

--- frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/Platform.java
+++ frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/Platform.java
@@ -1,12 +1,14 @@
 package com.github.eirslett.maven.plugins.frontend.lib;
 
-enum Architecture { x86, x64, ppc64le, arm64;
+enum Architecture { x86, x64, ppc64le, s390x, arm64;
     public static Architecture guess(){
         String arch = System.getProperty("os.arch");
         if (arch.equals("ppc64le")) {
             return ppc64le;
         } else if (arch.equals("aarch64")) {
             return arm64;
+        } else if (arch.equals("s390x")) {
+                return s390x;		
         } else {
             return arch.contains("64") ? x64 : x86;
         }
