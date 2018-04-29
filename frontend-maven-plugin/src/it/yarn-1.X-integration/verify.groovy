ea0885c2878f5077b517d18d2c8cf5d5197e5507
Michael Riehemann <michael.riehemann@coremedia.com>
2018-04-29 15:48:31 +0200

    Update libs for tests
    - remove separate yarn 1.x tests
    - update default yarn tests to 1.6.0
    - update package.json and node

--- frontend-maven-plugin/src/it/yarn-1.X-integration/verify.groovy
+++
@@ -1,7 +0,0 @@
-assert new File(basedir, 'target/node').exists() : "Node was not installed in the custom install directory";
-assert new File(basedir, 'node_modules').exists() : "Node modules were not installed in the base directory";
-assert new File(basedir, 'node_modules/less/package.json').exists() : "Less dependency has not been installed successfully";
-
-String buildLog = new File(basedir, 'build.log').text
-assert buildLog.contains('BUILD SUCCESS') : 'build was not successful'
-assert buildLog.replace(File.separatorChar, '/' as char).matches('(?s).+Unpacking .+\\Q/local-repo/com/github/eirslett/yarn/1.0.1/yarn-1.0.1.tar.gz\\E into .+/target/node/yarn.+') : 'incorrect local repository location'
