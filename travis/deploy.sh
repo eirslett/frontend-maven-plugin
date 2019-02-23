3728e5d284f3f7bcc1847dfabca8b5595f4fb309
Eirik <eiriksletteberg@gmail.com>
2019-02-23 16:04:42 +0100

    Try to configure Travis for Sonatype publishing

---
+++ travis/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+
+    openssl aes-256-cbc -K $encrypted_8c1bbe484fb8_key -iv $encrypted_8c1bbe484fb8_iv \
+      -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
+
+    gpg --fast-import travis/codesigning.asc
+    mvn --settings travis/settings.xml deploy -Prelease -DskipTests=true
+fi
