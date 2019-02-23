8e3cada2a71b8665c635df189709ab87771170c3
Eirik <eiriksletteberg@gmail.com>
2019-02-23 21:13:21 +0100

    only run deploy for linux

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
-openssl aes-256-cbc -K $encrypted_8c1bbe484fb8_key -iv $encrypted_8c1bbe484fb8_iv \
-  -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
+if [ "$TRAVIS_OS_NAME" == 'linux' ]; then
 
-gpg --fast-import travis/codesigning.asc
-mvn --settings travis/settings.xml deploy -Prelease,skipTests -DskipTests=true
+    openssl aes-256-cbc -K $encrypted_8c1bbe484fb8_key -iv $encrypted_8c1bbe484fb8_iv \
+      -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
+
+    gpg --fast-import travis/codesigning.asc
+    mvn --settings travis/settings.xml deploy -Prelease,skipTests -DskipTests=true
+fi
