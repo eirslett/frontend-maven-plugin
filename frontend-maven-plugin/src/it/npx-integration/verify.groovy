73ed1d8acc457912e67bfc5aecc361763aa0a256
Luigi Polvani <jimmyfm@users.noreply.github.com>
2020-03-23 12:06:30 -0500

    Fixed integration tests ...

--- frontend-maven-plugin/src/it/npx-integration/verify.groovy
+++ frontend-maven-plugin/src/it/npx-integration/verify.groovy
@@ -1,2 +1,2 @@
 String buildLog = new File(basedir, 'build.log').text
-assert buildLog.contains('< Hello >') : 'gulp failed to run as expected'
+assert buildLog.contains('< hello >') : 'gulp failed to run as expected'
