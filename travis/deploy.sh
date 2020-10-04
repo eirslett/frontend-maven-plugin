2021109f3dd822f7b28d07048f730c635588e1d8
Eirik Sletteberg <eirik.sletteberg@nav.no>
2020-10-04 22:12:53 +0200

    Remove Travis config

--- travis/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-if [ "$TRAVIS_OS_NAME" == 'linux' ]; then
-
-    openssl aes-256-cbc -K $encrypted_8c1bbe484fb8_key -iv $encrypted_8c1bbe484fb8_iv \
-      -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
-
-    gpg --fast-import travis/codesigning.asc
-    mvn --settings travis/settings.xml deploy -Prelease,skipTests -DskipTests=true
-fi
