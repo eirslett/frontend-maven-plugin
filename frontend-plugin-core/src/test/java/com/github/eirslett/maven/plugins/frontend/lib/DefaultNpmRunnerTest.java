3a1a13643f1557f855c26e1b7e3ee0042f456424
Thomas Kalmar <971164+tkalmar@users.noreply.github.com>
2020-10-07 12:44:16 +0200

    Fix proxy parsing

--- frontend-plugin-core/src/test/java/com/github/eirslett/maven/plugins/frontend/lib/DefaultNpmRunnerTest.java
+++ frontend-plugin-core/src/test/java/com/github/eirslett/maven/plugins/frontend/lib/DefaultNpmRunnerTest.java
@@ -19,8 +19,8 @@ public class DefaultNpmRunnerTest {
     private final int port = 8888;
     private final String username = "someusername";
     private final String password = "somepassword";
-    private final String nonProxyHosts = "www.google.ca|www.google.com";
-    private final String expectedNonProxyHosts = "www.google.ca,www.google.com";
+    private final String nonProxyHosts = "www.google.ca|www.google.com|*.google.de";
+    private final String[] expectedNonProxyHosts = new String[] {"www.google.ca","www.google.com",".google.de"};
     private final String registryUrl = "www.npm.org";
     private final String expectedUrl = "http://someusername:somepassword@localhost:8888";
 
@@ -30,9 +30,11 @@ public class DefaultNpmRunnerTest {
 
         assertThat(strings, CoreMatchers.hasItem("--proxy=" + expectedUrl));
         assertThat(strings, CoreMatchers.hasItem("--https-proxy=" + expectedUrl));
-        assertThat(strings, CoreMatchers.hasItem("--noproxy=" + expectedNonProxyHosts));
+        for (String expectedNonProxyHost: expectedNonProxyHosts) {
+            assertThat(strings, CoreMatchers.hasItem("--noproxy=" + expectedNonProxyHost));
+        }
         assertThat(strings, CoreMatchers.hasItem("--registry=" + registryUrl));
-        assertEquals(4, strings.size());
+        assertEquals(6, strings.size());
     }
 
 
@@ -42,8 +44,10 @@ public class DefaultNpmRunnerTest {
 
         assertThat(strings, CoreMatchers.hasItem("--proxy=" + expectedUrl));
         assertThat(strings, CoreMatchers.hasItem("--https-proxy=" + expectedUrl));
-        assertThat(strings, CoreMatchers.hasItem("--noproxy=" + expectedNonProxyHosts));
-        assertEquals(3, strings.size());
+        for (String expectedNonProxyHost: expectedNonProxyHosts) {
+            assertThat(strings, CoreMatchers.hasItem("--noproxy=" + expectedNonProxyHost));
+        }
+        assertEquals(5, strings.size());
     }
 
     @Test
@@ -52,8 +56,10 @@ public class DefaultNpmRunnerTest {
 
         assertThat(strings, CoreMatchers.hasItem("--proxy=" + expectedUrl));
         assertThat(strings, CoreMatchers.hasItem("--https-proxy=" + expectedUrl));
-        assertThat(strings, CoreMatchers.hasItem("--noproxy=" + expectedNonProxyHosts));
-        assertEquals(3, strings.size());
+        for (String expectedNonProxyHost: expectedNonProxyHosts) {
+            assertThat(strings, CoreMatchers.hasItem("--noproxy=" + expectedNonProxyHost));
+        }
+        assertEquals(5, strings.size());
     }
 
     @Test
