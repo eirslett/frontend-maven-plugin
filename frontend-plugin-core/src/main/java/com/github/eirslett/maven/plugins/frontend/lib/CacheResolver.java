692e152001ad1e750a96678a44e0dda200c2e232
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-20 22:02:54 +0800

    Delete frontend-plugin-core directory

--- frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/CacheResolver.java
+++
@@ -1,7 +0,0 @@
-package com.github.eirslett.maven.plugins.frontend.lib;
-
-import java.io.File;
-
-public interface CacheResolver {
-  File resolve(CacheDescriptor cacheDescriptor);
-}
--- frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/NodeTaskRunner.java
+++
@@ -1,7 +0,0 @@
-package com.github.eirslett.maven.plugins.frontend.lib;
-
-import java.util.Map;
-
-interface NodeTaskRunner {
-	void execute(String args, Map<String,String> environment) throws TaskRunnerException;
-}
