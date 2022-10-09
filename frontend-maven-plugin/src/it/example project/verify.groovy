2f472f0005066ce08b5b569d487a55a475ee9499
Eirik Sletteberg <eiriksletteberg@gmail.com>
2022-10-09 14:16:21 +0200

    Update verify script

--- frontend-maven-plugin/src/it/example project/verify.groovy
+++ frontend-maven-plugin/src/it/example project/verify.groovy
@@ -1,7 +1,5 @@
 import org.codehaus.plexus.util.FileUtils;
 
 String buildLog = FileUtils.fileRead(new File(basedir, 'build.log'));
-
-assert buildLog.contains('gulp runs as expected') : 'gulp failed to run as expected'
 assert buildLog.contains('all tests passed!') : 'web-test-runner failed to run as expected'
-assert buildLog.contains('4 modules transformed.') : 'build was not successful'
+assert buildLog.contains('7 modules transformed.') : 'build was not successful'
