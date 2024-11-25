2cb1f19e9f4e366a3d5c5a4acc0588ed948c47ef
Michael Kemp <mkemp@atlassian.com>
2024-11-26 00:24:30 +0100

    DCA11Y-1145: Treat snapshot files as source code

--- frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/IncrementalMojoHelper.java
+++ frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/IncrementalMojoHelper.java
@@ -152,6 +152,8 @@ public class IncrementalMojoHelper {
                 "mjs",
                 "ts",
                 "tsx",
+                // snapshots
+                ".snap",
                 // CSS
                 "css",
                 "scss",
